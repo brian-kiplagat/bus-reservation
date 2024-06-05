8b89f59357411a5d3f81ebac006576710f58b7d2
Brian Kiplagat <kiplagatbrian18@gmail.com>
2024-06-05 21:11:17 +0300

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,35 @@
+public class Car {
+    private String licensePlate;
+    private String model;
+    private boolean isRented;
+
+    public Car(String licensePlate, String model) {
+        this.licensePlate = licensePlate;
+        this.model = model;
+        this.isRented = false;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    public void rentCar() {
+        if (!isRented) {
+            isRented = true;
+        }
+    }
+
+    public void returnCar() {
+        if (isRented) {
+            isRented = false;
+        }
+    }
+}
